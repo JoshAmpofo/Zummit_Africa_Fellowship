af2f640447a2a2def8b77926d18bb1c0c6a023db
Joshua Ampofo Yentumi <joshampofo92@gmail.com>
2024-02-17 13:36:43 +0000

    write unittests for twttr function

---
+++ python_track/Week5-Unit_tests/test_twttr/test_twttr.py
@@ -0,0 +1,62 @@
+"""
+Author: Joshua Ampofo Yentumi
+
+PROBLEM SET 5
+
+Problem 1: Testing my twttr
+
+Description: Implement one or more functions that collectively test the implementation
+             of "shorten" thoroughly, each of whose names should begin with "test_" 
+             so that tests can be executed with:
+             "pytest test_twttr.py.
+"""
+
+from twttr import shorten
+
+
+def test_argument_uppercase():
+    """Test if function returns str in uppercase without vowels"""
+    assert shorten("MADAM") == "MDM"
+    assert shorten("MEDICINE") == "MDCN"
+    assert shorten("TWITTER") == "TWTTR"
+
+
+def test_argument_lowercase():
+    """Test if function returns str in lowercase without vowels"""
+    assert shorten("twitter") == "twttr"
+    assert shorten("medicine") == "mdcn"
+    assert shorten("jobs") == "jbs"
+
+
+def test_argument_mixed_case():
+    """Test if function returns str in mixed case without vowels"""
+    assert shorten("twiTTeR") == "twTTR"
+    assert shorten("MaNdAriN") == "MNdrN"
+
+
+def test_longer_str():
+    """Test if function can return longer strings without vowels"""
+    assert shorten("Hello World") == "Hll Wrld"
+    assert shorten("hello world") == "hll wrld"
+    assert shorten("i love python") == " lv pythn"
+
+
+def test_single_character():
+    """Test if function can handle string with only one character"""
+    assert shorten("a") == ""
+    assert shorten("d") == "d"
+
+
+def test_vowels_only():
+    """Test if function can handle only vowels"""
+    assert shorten("aeiou") == ""
+
+
+def test_full_alphabet():
+    """Test if function can handle full alphabet"""
+    assert shorten("abcdefghijklmnopqrstuvwxyz") == "bcdfghjklmnpqrstvwxyz"
+
+
+def test_empty_strin():
+    """Test if function returns an empty string when given an empty string"""
+    assert shorten("") == ""
---
+++ python_track/Week5-Unit_tests/test_twttr/twttr.py
@@ -0,0 +1,34 @@
+"""
+Author: Joshua Ampofo Yentumi
+
+PROBLEM SET 5
+
+Problem 1: Testing my twttr
+
+Description: Reimplement Setting up my twttr from Problem Set 2, restructuring the code per the below
+             wherein "shorten" expects a "str" as input and returns that same "str" but with all the vowels
+             (A,E, I, O, U) omitted, whether inputted in uppercase or lowercase.
+"""
+
+
+def main():
+    text = input("Input text: ").strip()
+    print(shorten(text))
+
+
+def shorten(text):
+    """Remove all vowels from input text"""
+    VOWELS = ['A', 'E', 'I', 'O', 'U']
+
+    # create empty string to store new word
+    new_word = ""
+
+    # iterate through each character in word
+    for char in text:
+        if char.upper() not in VOWELS: # check if word is not in vowels list
+            new_word += char # form ne word
+    return new_word
+
+
+if __name__ == '__main__':
+    main()
