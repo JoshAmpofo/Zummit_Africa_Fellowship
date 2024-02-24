a63dde03015df7f2fc266059a00fc16facce5932
Joshua Ampofo Yentumi <joshampofo92@gmail.com>
2024-02-24 17:14:42 +0000

    test files for count_lines function

---
+++ python_track/Week6-File_IO/lines/test.py
@@ -0,0 +1,10 @@
+# Say hello
+
+"""
+Testing lines.py program
+Here are some more lines
+Here are some even more lines
+Here are more more more lines
+"""
+name = input("What's your name? ")
+print(f"hello, {name}")
